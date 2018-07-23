9f661d397e2b945bd338a7508cb2f6cb678a2f84
Krzysztof Stefanski <krzysztof.stefanski82@gmail.com>
2018-07-23 11:26:40 +0200

    deteted test.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+//imports from npm packages
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+
+//import external resources
 import './index.css'
+
+
+
+//import my own componets
 import App from './App'
 
 ReactDOM.render(
 <App />,
- document.getElementById('root'))
+ document.getElementById('root')
+)
+
 
 
 
