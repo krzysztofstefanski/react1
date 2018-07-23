6a12475fc1859b0f201612d4d2fc15c996ee6179
Krzysztof Stefanski <krzysztof.stefanski82@gmail.com>
2018-07-23 11:48:23 +0200

    Changing component to function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+
+const App = () =>(
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -15,7 +14,5 @@ class App extends Component {
         </p>
       </div>
     );
-  }
-}
 
 export default App;
