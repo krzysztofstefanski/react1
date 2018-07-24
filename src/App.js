908c778956829c651610661c90cd6b76113ce00b
Krzysztof Stefanski <krzysztof.stefanski82@gmail.com>
2018-07-24 13:39:50 +0200

    props.match.params

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-import {BrowserRouter as Router, Route,Link} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 
 import BasicComponents from './BasicComponents/BasicComponents'
 import Dashboard from './Dashboard'
+import PassingParameters from './PassingParameters'
+
 
 const App = () => (
 
@@ -11,11 +14,13 @@ const App = () => (
       <div>
         <Link to={'./dashboard'}>Dashboard</Link>
         <Link to={'./basic-components'}>Basic Components</Link>
-<Route path={'/'}exact component={Dashboard}/>
-<Route path={'/dashboard'} component={Dashboard}/>
-<Route path={'/basic-components'} component={BasicComponents}/>
-</div>
-      </Router>
+        <Link to={'/passing-parameters/cos'}>Parametr - coś</Link>
+        <Route path={'/'} exact component={Dashboard} />
+        <Route path={'/dashboard'} component={Dashboard} />
+        <Route path={'/basic-components'} component={BasicComponents} />
+        <Route path={'/passing-parameters/:parameterName'} component={PassingParameters} />
+      </div>
+    </Router>
   </div>
 )
 
